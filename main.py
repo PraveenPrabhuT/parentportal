8d2a57a280c416b81644386120f69e8f5d29c141
PraveenPrabhuT <t.praveenprabhu5@gmail.com>
2022-02-05 15:19:38 +0530

    Project Completed

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 # Importing Necessary Modules
 import mysql.connector
 import csv
+from prettytable import PrettyTable
 
 # Login Function
 access=0
+user='default'
 def login():
+    global user
     user = input("Enter Username: ")
     password= input("Enter Password: ")
     users = {"praveen":"password1", "sachin":"password2","aravind":"password3"}
     keyz=users.keys()
     if user in keyz:
         if users[user]==password:
-            global login
-            login=1
-            print("Success")
+            global access 
+            access=1
+            print("You have logged in successfully")
         else:
             print("Enter correct password")
     else:
@@ -44,7 +47,6 @@ def fee_receipt():
     detail=mycursor.fetchall()
     if len(detail)==0:
         print("Roll No not found")
-        break
     else:
         f.write("         ABC INTERNATIONAL SCHOOL \n")
         f.write("             FEE RECEIPT \n\n")
@@ -73,6 +75,8 @@ def holiday_list():
 def feedback():
     f=open('feedback.txt','a')
     s=input('Kindly give your valuable feedback:')
+    global user
+    s=s+' given by '+str(user)+'\n'
     f.write(s)
     f.close()
 
@@ -80,13 +84,15 @@ def feedback():
 def class_diary():
     f=open('class_diary.txt','r')
     diary=f.read()
-    print (diary)
+    print("\t\t\tCLASS DIARY")
+    print(diary)
     f.close()
 
 #Assignment Details Function
 def assignment_details():
     f=open('assignment_details.txt','r')
     assignments=f.read()
+    print("\t\t\tASSIGNMENT DETAILS")
     print(assignments)
     f.close()
 
@@ -94,22 +100,36 @@ def assignment_details():
 def announcements():
     f=open('announcements.txt','r')
     announcements=f.read()
+    print("\t\t\tANNOUNCEMENTS")
     print(announcements)
     f.close()
 
 #Attendance Function
 def attendance():
-    f=open('attendance.txt','r')
-    attendance=f.read()
-    print(attendance)
+    global user
+    f=open('attendance.csv','r')
+    attendance=csv.reader(f)
+    table=PrettyTable()
+    table_title='Attendance'+' Name :'+str(user)
+    table.title=table_title
+    table.field_names=(['Mon','Tue','Wed','Thu','Fri','Sat'])
+    for row in attendance:
+        table.add_row(row)
+    table.align="c"
+    print(table)
     f.close()
 
 #Timetable Function
 def timetable():
     f=open('timetable.csv','r')
-    timetable=csv.reader(f)
-    for row in timetable:
-        print(row)
+    time_table=csv.reader(f)
+    table=PrettyTable()
+    table.title='Class Timetable'
+    table.field_names=(['Day','09 - 10','10 - 11','11 - 12','12 - 01','01 - 02','02 - 03'])
+    for row in time_table:
+        table.add_row(row)
+    table.align="c"
+    print(table)
     f.close()
 
 #Menu
@@ -126,34 +146,34 @@ def menu():
         9. Feedback
         0. Exit
     ''')
+    ch=int(input('Enter your choice:'))
+    if ch==1:
+        class_diary()
+    elif ch==2:
+        assignment_details()
+    elif ch==3:
+        announcements()
+    elif ch==4:
+        attendance()
+    elif ch==5:
+        timetable()   
+    elif ch==6:
+        fee_details()
+    elif ch==7:
+        fee_receipt()
+    elif ch==8:
+        holiday_list()
+    elif ch==9:
+        feedback()
+    elif ch==0:
+        global stop
+        stop=1
+    else:
+        print("Please enter between 1 to 9")
 
-while access=0:
+while access==0:
     login()
 
 stop=0
 while stop==0:
     menu()
-    ch=int(input('Enter your choice:'))
-
-if ch==1:
-    class_diary()
-elif ch==2:
-    assignment()
-elif ch==3:
-    announcement()
-elif ch==4:
-    attendance()
-elif ch==5:
-    timetable()   
-elif ch==6:
-    fee_details()
-elif ch==7:
-    fee_receipt()
-elif ch==8:
-    holiday_list()
-elif ch==9:
-    feedback()
-elif ch==0:
-    stop=1
-else:
-    print("Please enter between 1 to 9")
