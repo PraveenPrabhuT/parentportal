361fa59c6b7b516a1b53a1474fe69c19cc618b20
PraveenPrabhuT <t.praveenprabhu5@gmail.com>
2022-02-11 17:53:18 +0530

    Finishing changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Importing Necessary Modules
+
 import mysql.connector
 import csv
 import stdiomask
@@ -6,20 +7,21 @@ import getpass
 from prettytable import PrettyTable
 
 # Login Function
+
 access = 0
 user = 'default'
 def sqlLogin():
     global user
     user = input ("Enter Username: ")
     password = stdiomask.getpass(prompt = "Enter Password: ")
-    login_details = mysql.connector.connect(host = "localhost",user = "root",passwd = "root",database = "loginDetails")
-    mycursor = login_details.cursor()
+    mysql_connection = mysql.connector.connect(host = "localhost",user = "root",passwd = "root",database = "loginDetails")
+    mycursor = mysql_connection.cursor()
     query = "SELECT password FROM login WHERE userID = \'"+user+"\';"
     mycursor.execute(query)
-    detail = mycursor.fetchall()
-    CorrectPassword = str(detail[0])
+    loginDetails = mycursor.fetchall()
+    CorrectPassword = str(loginDetails[0])
     CorrectPassword = CorrectPassword[2:-3]
-    if len(detail) == 0:
+    if len(loginDetails) == 0:
         print("User not found")
     else:
         if password == CorrectPassword:
@@ -28,113 +30,129 @@ def sqlLogin():
             access = 1
         else:
             print("Enter correct password")
+
 # Fee Details Function
+
 def fee_details():
-    fee_detail = mysql.connector.connect(host = "localhost", user = "root", passwd = "root",database = "feeDetails")
-    mycursor = fee_detail.cursor()
+    mysql_connection = mysql.connector.connect(host = "localhost", user = "root", passwd = "root",database = "feeDetails")
+    mycursor = mysql_connection.cursor()
     rollNo = int(input("Enter roll number"))
     query = "SELECT * FROM fees WHERE roll = {}".format(rollNo)
     mycursor.execute(query)
-    detail = mycursor.fetchall()
-    if len(detail) == 0:
+    feeDetails = mycursor.fetchall()
+    if len(feeDetails) == 0:
         print("Roll No not found")
     else:
-        print(detail)
+        print("Name         :",feeDetails[1])
+        print("Class        :",feeDetails[2])
+        print("Amount Paid  :",feeDetails[3])
+        print("Payment Mode :",feeDetails[4])
+        print("Due left     :",50000-feeDetails[3])
     mycursor.close()
 
 # Fee Receipt Generator Function
+
 def fee_receipt():
-    f = open('fee_receipt.txt','w')
+    file = open('fee_receipt.txt','w')
     rollNo = int(input('Enter roll number:'))
-    fee_detail = mysql.connector.connect(host = "localhost", user = "root", passwd = "root",database = "feeDetails")
-    mycursor = fee_detail.cursor()
+    mysql_connection = mysql.connector.connect(host = "localhost", user = "root", passwd = "root",database = "feeDetails")
+    mycursor = mysql_connection.cursor()
     query = "SELECT * FROM fees WHERE roll = {}".format(rollNo)
     mycursor.execute(query)
-    detail = mycursor.fetchall()
-    if len(detail) == 0:
+    feeDetails = mycursor.fetchall()
+    if len(feeDetails) == 0:
         print("Roll No not found")
     else:
-        detailTuple = tuple(detail[0])
+        detailTuple = tuple(feeDetails[0])
         f.write("         ABC INTERNATIONAL SCHOOL \n")
         f.write("             FEE RECEIPT \n\n")
         line1 = ("Name         :  " + str(detailTuple[1]) + "\n")
         line2 = ("Class        :  " + str(detailTuple[2]) + "\n")
         line3 = ("Received     :  " + str(detailTuple[3]) + "\n")
         line4 = ("Payment mode :  " + str(detailTuple[4]) + "\n")
-        f.write(line1)
-        f.write(line2)
-        f.write(line3)
-        f.write(line4)
-        f.write("\nAddress : #2, Abc street, 560043\n")
-        f.write("Phone no: 1800-XXX-XXX")
-        f.close()
+        due_left = 50000 - detailTuple[3]
+        line5 = ("Due left     :  " + due_left] + "\n")
+        file.write(line1)
+        file.write(line2)
+        file.write(line3)
+        file.write(line4)
+        file.write(line5)
+        file.write("\nAddress : #2, Abc street, 560043\n")
+        file.write("Phone no: 1800-XXX-XXX")
+        file.close()
         print("The receipt is generated as fee_receipt.txt")
     mycursor.close()
 
 #Holiday List Function
+
 def holiday_list():
-    f = open('holiday_list.txt','r')
-    x = f.read()
-    print(x)
-    f.close()
+    file = open('holiday_list.txt','r')
+    HolidayList = file.read()
+    print(HolidayList)
+    file.close()
 
 #Feedback Function
+
 def feedback():
-    f = open('feedback.txt','a')
-    s = input('Kindly give your valuable feedback:')
+    file = open('feedback.txt','a')
+    userFeedback = input('Kindly give your valuable feedback:')
     global user
-    s = s+' given by '+str(user)+'\n'
-    f.write(s)
-    f.close()
+    userFeedback = userFeedback+' given by '+str(user)+'\n'
+    file.write(s)
+    file.close()
 
 #Class Diary Function
+
 def class_diary():
-    f = open('class_diary.txt','r')
-    diary = f.readlines()
+    file = open('class_diary.txt','r')
+    ClassDiary = file.readlines()
     print("\t\t\tCLASS DIARY")
-    for i in diary:
+    for i in ClassDiary:
         diaryContent = i.split("|")
         recepient = diaryContent[0].strip()
         if recepient in ['General',user]:
             print(diaryContent[1])
-    f.close()
+    file.close()
 
 #Assignment Details Function
+
 def assignment_details():
-    f = open('assignment_details.txt','r')
-    assignments = f.read()
+    file = open('assignment_details.txt','r')
+    assignmentDetails = file.read()
     print("\t\t\tASSIGNMENT DETAILS")
-    print(assignments)
-    f.close()
+    print(assignmentDetails)
+    file.close()
 
 #Announcements Function
+
 def announcements():
-    f = open('announcements.txt','r')
-    announcements = f.read()
+    file = open('announcements.txt','r')
+    announcements = file.read()
     print("\t\t\tANNOUNCEMENTS")
     print(announcements)
-    f.close()
+    file.close()
 
 #Attendance Function
+
 def attendance():
     global user
-    f = open('attendance.csv','r')
-    attendance = csv.reader(f)
+    file = open('attendance.csv','r')
+    attendance = csv.reader(file)
     table = PrettyTable()
-    table_title = 'Attendance'
-    table.title = table_title
+    table.title = 'Attendance'
     table.field_names = (['Name','Mon','Tue','Wed','Thu','Fri','Sat'])
     for row in attendance:
         if row[0] == user:
             table.add_row(row)
     table.align = "c"
     print(table)
-    f.close()
+    file.close()
 
 #Timetable Function
+
 def timetable():
-    f = open('timetable.csv','r')
-    time_table = csv.reader(f)
+    file = open('timetable.csv','r')
+    time_table = csv.reader(file)
     table = PrettyTable()
     table.title = 'Class Timetable'
     table.field_names = (['Day','09 - 10','10 - 11','11 - 12','12 - 01','01 - 02','02 - 03'])
@@ -142,11 +160,13 @@ def timetable():
         table.add_row(row)
     table.align = "c"
     print(table)
-    f.close()
+    file.close()
 
 #Menu
+
 def menu():
     print('''
+        WELCOME TO PARENT PORTAL
         1. Class Diary
         2. Assignment
         3. Announcements
@@ -158,30 +178,30 @@ def menu():
         9. Feedback
         0. Exit
     ''')
-    ch = int(input('Enter your choice:'))
-    if ch == 1:
+    choice = int(input('Enter your choice:'))
+    if choice == 1:
         class_diary()
-    elif ch == 2:
+    elif choice == 2:
         assignment_details()
-    elif ch == 3:
+    elif choice == 3:
         announcements()
-    elif ch == 4:
+    elif choice == 4:
         attendance()
-    elif ch == 5:
+    elif choice == 5:
         timetable()   
-    elif ch == 6:
+    elif choice == 6:
         fee_details()
-    elif ch == 7:
+    elif choice == 7:
         fee_receipt()
-    elif ch == 8:
+    elif choice == 8:
         holiday_list()
-    elif ch == 9:
+    elif choice == 9:
         feedback()
-    elif ch == 0:
+    elif choice == 0:
         global stop
         stop = 1
     else:
-        print("Please enter between 1 to 9")
+        print("Please enter a number between 1 to 9")
 
 while access == 0:
     sqlLogin()
